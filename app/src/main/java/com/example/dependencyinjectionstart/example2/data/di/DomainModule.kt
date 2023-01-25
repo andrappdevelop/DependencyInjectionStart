5347df8226a2ac19ec15cb3f8da3a208d2094c39
IvanUsatov <andrappdevelop@gmail.com>
2023-01-25 20:09:42 +0700

    add dagger

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/di/DomainModule.kt
@@ -0,0 +1,12 @@
+package com.example.dependencyinjectionstart.example2.data.di
+
+import com.example.dependencyinjectionstart.example2.data.repository.ExampleRepositoryImpl
+import com.example.dependencyinjectionstart.example2.domain.ExampleRepository
+import dagger.Binds
+import dagger.Module
+
+@Module
+interface DomainModule {
+    @Binds
+    fun bindRepository(impl: ExampleRepositoryImpl): ExampleRepository
+}
