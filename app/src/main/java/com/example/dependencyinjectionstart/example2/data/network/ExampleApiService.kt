444958b763a2651529dee06cd6d80c0996ea073a
IvanUsatov <andrappdevelop@gmail.com>
2023-01-26 20:06:56 +0700

    edit with CustomScope

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
@@ -3,16 +3,19 @@ package com.example.dependencyinjectionstart.example2.data.network
 import android.content.Context
 import android.util.Log
 import com.example.dependencyinjectionstart.R
+import com.example.dependencyinjectionstart.example2.data.di.ApplicationScope
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
+@ApplicationScope
 class ExampleApiService @Inject constructor(
     private val context: Context,
     private val timeMillis: Long
 ) {
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)} $timeMillis $this")
+        Log.d(
+            LOG_TAG,
+            "ExampleApiService ${context.getString(R.string.app_name)} $timeMillis $this"
+        )
     }
 
     companion object {
