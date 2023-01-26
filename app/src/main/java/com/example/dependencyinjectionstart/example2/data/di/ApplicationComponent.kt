228443a918cfa357ea723e7a5808ee2b59ff6898
IvanUsatov <andrappdevelop@gmail.com>
2023-01-26 19:46:11 +0700

    edit with Singletone

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/di/ApplicationComponent.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
+
     fun inject(activity: MainActivity)
 
     @Component.Factory
